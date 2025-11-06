c0e785853b25055d1c519dc0e0f30a73ffce8d1e
kixixixixi <kixixixixi@gmail.com>
2025-11-06 19:13:26 +0900

    Initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,17 @@
+import "reset.css"
+
+export const metadata = {
+  title: "Page title",
+  description: "Page description",
+}
+
+const RootLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <html lang="ja">
+      <body>
+        <main>{children}</main>
+      </body>
+    </html>
+  )
+}
+export default RootLayout
