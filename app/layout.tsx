8263242051dafafd75f8fae96223c79d646f4fd6
kixixixixi <kixixixixi@gmail.com>
2025-11-06 19:25:05 +0900

    Set title and header

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,24 @@
-import "reset.css"
+import "./reset.css"
 
 export const metadata = {
-  title: "Page title",
-  description: "Page description",
+  title: "MyTL",
+  description: "MyTLは自分の年表をつくって共有できるサイト",
 }
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="ja">
       <body>
+        <header
+          style={{
+            backgroundColor: "#f9f9f9",
+            boxShadow: "0 4px 6px rgba(0, 0, 0, 0.1)",
+            padding: ".5rem",
+            position: "relative",
+          }}
+        >
+          <h1 style={{ fontSize: "1rem" }}>MyTL</h1>
+        </header>
         <main>{children}</main>
       </body>
     </html>
